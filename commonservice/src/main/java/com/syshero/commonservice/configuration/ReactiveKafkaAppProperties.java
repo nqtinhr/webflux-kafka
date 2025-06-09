91a1ac3384d3b029a427cfa1a58d805fbc2f92ff
nqtinhr <itnetwork102@gmail.com>
2025-06-09 23:16:20 +0700

    config kafka

---
+++ commonservice/src/main/java/com/syshero/commonservice/configuration/ReactiveKafkaAppProperties.java
@@ -0,0 +1,14 @@
+package com.syshero.commonservice.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ReactiveKafkaAppProperties {
+    @Value("${kafka.bootstrap.servers}")
+    String bootstrapServers;
+
+    @Value("${payment.kafka.consumer-group-id}")
+    String consumerGroupId;
+}
+
